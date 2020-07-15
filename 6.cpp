90ea813d105267bba66ac3b4890feaf0c0a36ee9
Naman Goyal <60535864+Whistler27@users.noreply.github.com>
2020-07-15 13:32:32 +0530

    Doctor Chef

---
+++ 6.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+ 
+#define endl '\n'
+#define loop(i, m, n) for (ll i = m; i < n; i++)
+#define loope(i, m, n) for (ll i = m; i <= n; i++)
+#define epool(i, m, n) for (ll i = m; i >= n; i--)
+#define mod 1000000007
+#define mod2 998244353
+#define vi vector<ll>
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define pb push_back
+#define MP make_pair
+#define empf emplace_front
+#define empb emplace_back
+#define ret(x) return cout << x,0;
+#define all(v) v.begin(),v.end()
+#define maxheap priority_queue<ll>
+#define minheap priority_queue<ll,vector<ll>,greater<ll>>
+const ll INF=1e18;
+const double PI=4*atan(1);
+
+#define SpeedForce                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+using namespace std;
+
+void solve(){
+    ll n,x;
+    cin >> n >> x;
+    vector<ll> a(n);
+    loop(i,0,n){
+        cin >> a[i];
+    }
+    sort(all(a));
+
+
+    ll high = a[n-1]*2;
+    ll low=x;
+
+    bool f[n+1];
+    memset(f,0,sizeof(f));
+
+    ll cur=0;
+    while(high > low){
+        
+        cur++;
+        high=(high+1)/2;
+
+        ll ind = lower_bound(all(a),high)-a.begin();
+        if(f[ind]==0){
+                f[ind] = 1;
+                high = a[ind];
+        }
+    }
+
+    loop(i,0,n){
+        if(f[i]==0) cur++;
+    }
+    
+    cout << cur << endl;
+
+}
+
+int32_t main(){
+    SpeedForce
+    ll i = 0,j = 0;
+
+
+    int t;
+    t=1;
+    cin >> t;
+    while (t--){
+        //cout << fixed << setprecision(10);
+        solve();
+    }
+}
